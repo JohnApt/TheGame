da518100066ea990a0bfd09acbb789341ccbf345
Benji Altman <benji@altmansoftwaredesign.com>
2022-06-19 20:43:57 -0500

    linegraph

--- GraphGenerator/Program.cs
+++ GraphGenerator/Program.cs
@@ -4,11 +4,14 @@ Graph MakeMainLineWithLoops(uint length)
 {
 	var graph = new Graph();
 
-	for(int i = 0; i < length; i++)
+	// Make main line.
+	for (uint i = 1; i < length; i++)
 	{
-
+		graph.AddBidirectionalEdge(i, i + 1);
 	}
-
 
 	return graph;
 }
+
+
+Console.WriteLine(MakeMainLineWithLoops(10).Printout());
