6ca924f18991b620489de252344807d8e63d6cdf
Benji Altman <benji@altmansoftwaredesign.com>
2022-06-19 20:16:42 -0500

    add graph structure

---
+++ GraphGenerator/Graph.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+	public class Graph
+	{
+		/// <summary>
+		/// ith index in the list has a hashset of the vertcies that it shares an edge with.
+		/// </summary>
+		private readonly List<HashSet<uint>> adjList = new();
+
+		public void AddEdge(uint a, uint b)
+		{
+			while(adjList.Count <= a)
+				adjList.Add(new());
+
+			adjList[(int)a].Add(b);
+		}
+
+		public void AddBidirectionalEdge(uint a, uint b)
+		{
+			AddEdge(a, b);
+			AddEdge(b, a);
+		}
+
+		public string Printout()
+		{
+			var sb = new StringBuilder();
+			for(int i = 0; i < adjList.Count; i++)
+				foreach (var other in adjList[i])
+					sb.Append($"{i} {other}");
+			return sb.ToString();
+		}
+
+		public bool HasPointOfNoReturn()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
